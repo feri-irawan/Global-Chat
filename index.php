afc748adb406f5729e3b67d0cc45488865bb3940
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-05-20 11:37:57 +0800

    update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (isset($_COOKIE["username"])) {
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
  
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/push.js/1.0.8/push.min.js" type="text/javascript" charset="utf-8"></script>
+  <script src="https://cdn.jsdelivr.net/npm/push-js@1.1.1/dist/Push.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js"></script>
 
 
