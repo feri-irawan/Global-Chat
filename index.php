f404c480533a1c55ae97c3ff6b17372a5368d2da
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-05-20 12:06:56 +0800

    update index.php

--- index.php
+++ index.php
@@ -166,7 +166,6 @@ if (isset($_COOKIE["username"])) {
         </div>
         <div class="ps-3 d-flex align-items-bottom">
           <button type="submit" id="btn-send" class="btn btn-primary">Send</button>
-          <a href="#" onclick="pushnotification()" class="btn ms-3 btn-success">Push</a>
         </div>
       </form>
     </main>
