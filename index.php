86045e998865acb85fbcf556a0b4dbc15ff8882d
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-05-20 13:17:26 +0800

    update index.php

--- index.php
+++ index.php
@@ -134,7 +134,7 @@ if (isset($_COOKIE["username"]) !== null) {
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">SAYHAIi</a>
+      <a class="navbar-brand" href="#">SayHaii</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
