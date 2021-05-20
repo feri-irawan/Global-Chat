2238dc53b31404da85839c96820eb4ab37f09098
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-05-20 12:02:23 +0800

    update index.php

--- index.php
+++ index.php
@@ -152,7 +152,7 @@ if (isset($_COOKIE["username"])) {
           <textarea name="chat" id="chat-input" rows="1" class="form-control" required></textarea>
         </div>
         <div class="ps-3 d-flex align-items-bottom">
-          <button type="submit" class="btn btn-primary">Send</button>
+          <button type="submit" id="btn-send" class="btn btn-primary">Send</button>
           <a href="#" onclick="pushnotification()" class="btn ms-3 btn-success">Push</a>
         </div>
       </form>
@@ -171,8 +171,15 @@ if (isset($_COOKIE["username"])) {
     $("#chat-form").submit(function (e) {
 
       let chat = $("#chat-input").val();
-      console.log(chat)
-
+      let btnSend = $("#btn-send");
+   
+      btnSend.html(`
+      <svg xmlns="http://www.w3.org/2000/svg" class="loading-spin" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+        <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+      </svg>
+      `)
+      
       sendChat(chat);
 
       e.preventDefault();
