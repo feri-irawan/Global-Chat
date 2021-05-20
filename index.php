ef04fda685fcf8007563402e6dc1d21d6b8b7095
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-05-20 11:58:41 +0800

    update index.php

--- index.php
+++ index.php
@@ -162,24 +162,8 @@ if (isset($_COOKIE["username"])) {
 
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/push-js@1.1.1/dist/Push.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js"></script>
-
+  
   <script>
-    function pushnotification() {
-    Push.create('Hello world!', {
-        body: 'How\'s it hangin\'?',
-        icon: '/images/icon.png',
-        link: '/#',
-        timeout: 4000,
-        onClick: function () {
-            console.log("Fired!");
-            window.focus();
-            this.close();
-        },
-        vibrate: [200, 100, 200, 100, 200, 100, 200]
-    });
-}
-
     setInterval(function () {
       getChat()
     }, 1500);
