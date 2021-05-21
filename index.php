4d2a36bb8fe197695f4c3012140dbc4e1b84f8c3
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-05-21 18:16:05 +0800

    update index.php

--- index.php
+++ index.php
@@ -403,6 +403,25 @@ if (isset($_COOKIE["username"]) != "" || isset($_SESSION["username"]) != "") {
       function konsol(val) {
         return console.log(val)
       }
+
+
+      // program to get a random item from an array
+
+      function getRandomItem(arr) {
+
+        // get random index value
+        const randomIndex = Math.floor(Math.random() * arr.length);
+
+        // get random item
+        const item = arr[randomIndex];
+
+        return item;
+      }
+
+      const array = [1, 'hello', 5, 8];
+
+      const result = getRandomItem(array);
+      console.log(result);
     </script>
     <?php endif; ?>
   </body>
