301380c29e83c2de6c62af3e1c5dabdc7c4b5219
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-05-21 09:53:50 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
+session_start();
+
 if (isset($_POST["username"])) {
+  $_SESSION["username"] = $_POST["username"];
   setcookie("username", $_POST["username"], time() + (7 * 24 * 60 * 60));
   header("location: index.php");
 }
 
 if (isset($_GET["logout"])) {
+  session_destroy();
   setcookie("username", "", time()-3600);
   header("location: index.php");
 }
 
-if (isset($_COOKIE["username"]) != "") {
+if (isset($_COOKIE["username"]) != "" || isset($_SESSION["username"]) != "") {
   $loginStatus = true;
 }
 ?>
