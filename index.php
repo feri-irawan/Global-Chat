78411072b27cfce640e8306e34faaa1825fbf7e4
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-05-22 14:10:40 +0800

    update index.php

--- index.php
+++ index.php
@@ -6,14 +6,14 @@ if (isset($_POST["username"])) {
   setcookie("username", $_POST["username"], time() + (7 * 24 * 60 * 60));
   setcookie("sayhaii_id", uniqid(""), time() + (7 * 24 * 60 * 60));
 
-  header("location: ".$_SERVER["HTTP_HOST"]);
+  header("location: //".$_SERVER["HTTP_HOST"]);
 }
 
 if (isset($_GET["logout"])) {
   session_destroy();
   setcookie("username", "", time()-3600);
   setcookie("sayhaii_id", "", time()-3600);
-  header("location: ".$_SERVER["HTTP_HOST"]);
+  header("location: //".$_SERVER["HTTP_HOST"]);
 }
 
 if (isset($_COOKIE["username"]) != "" || isset($_SESSION["username"]) != "") {
