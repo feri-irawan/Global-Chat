8b5ee5939bf783c9c1383f9c5d6c3d9b265b27a0
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-05-22 17:31:24 +0800

    update index.php

--- index.php
+++ index.php
@@ -25,13 +25,13 @@ if (isset($_COOKIE["username"]) != "" || isset($_SESSION["username"]) != "") {
 <html lang="en" translate="no">
 <head>
   <!--
-               * =============================
-               * Do not copy without permission
-               * Made by Feri Irawan, Indonesia
-               * Copyright 2021
-               * All rights reserved
-               * =============================
-              -->
+                 * =============================
+                 * Do not copy without permission
+                 * Made by Feri Irawan, Indonesia
+                 * Copyright 2021
+                 * All rights reserved
+                 * =============================
+                -->
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
@@ -118,7 +118,7 @@ if (isset($_COOKIE["username"]) != "" || isset($_SESSION["username"]) != "") {
         </div>
         <div class="ps-3 d-flex align-items-bottom">
           <button type="submit" id="btn-send" class="btn btn-primary">Send</button>
-          <button onclick="getChat()" class="btn btn-success">Get</button>
+          <button type="button" onclick="getChat()" class="btn btn-success">Get</button>
         </div>
       </form>
     </main>
