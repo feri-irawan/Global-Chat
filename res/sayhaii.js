b4e6499c3c1f4681288a8d4d0e8136d4ed187d24
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-05-22 11:57:58 +0800

    update sayhaii.js

--- res/sayhaii.js
+++ res/sayhaii.js
@@ -154,6 +154,7 @@ function getChat() {
       $("#chat-container").html(res);
 
       $(".sayhaii-"+ Cookies.get("sayhaii_id")).addClass("chat-box-right");
+      $(".box-id-"+ Cookies.get("sayhaii_id")).css("right", "0");
 
       var btnSend = $("#btn-send");
       btnSend.html("Send");
